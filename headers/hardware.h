3d312ac1da76926ac793243f774672cbdcd1753a
eSuardOdin <esuardodin@gmail.com>
2025-03-23 21:55:38 +0100

    Done everything but audio

--- headers/hardware.h
+++ headers/hardware.h
@@ -1,7 +1,7 @@
 /**
  * @file hardware.h
  * @author Erwann Suard
- * @brief Define constant adresses used to access special "registers" in RAM
+ * @brief Define constant adresses used to access special "registers" in RAM - Doing only DMG (original GB)
  * @version 0.1
  * @date 2025-03-23
  * 
@@ -77,4 +77,83 @@
  * @brief Interrupt Enable - Same bit order as IF (FF0F)
  */
 #define IE 0xFFFF
+
+
+// #############
+// ### Audio ###
+// #############
+// Oui ben we'll see mais it will wait brother
+// 0xFF10 - 0xFF3F
+
+
+
+// #####################
+// ### LCD Registers ###
+// #####################
+
+/**
+ * @brief LDC Control - Manages what elements are printed on screen and how
+ * @details Check doc at https://gbdev.io/pandocs/LCDC.html#ff40--lcdc-lcd-control
+ */
+#define LCDC 0xFF40 
+
+/**
+ * @brief LCD Status - Check doc
+ */
+#define STAT 0xFF41
+
+/**
+ * @brief Background Viewport Y - Topleft Y axis of visible area in BG map.
+ */
+#define SCY 0xFF42
+
+/**
+ * @brief Background Viewport X - Topleft X axis of visible area in BG map.
+ */
+#define SCX 0xFF43
+
+/**
+ * @brief Window Y Position - Onscreen coordinate of the window's Y top left pixel.
+ */
+#define WY 0xFF4A
+
+/**
+ * @brief Window X Position - Onscreen coordinate of the window's X top left pixel.
+ */
+ #define WX 0xFF4B
+
+/**
+ * @brief LCD Y coordinate [READ ONLY] - Indicate line to draw, drawing or just been drawn.
+ */
+#define LY 0xFF44
+
+/**
+ * @brief LY Compare - GameBoy checks if LYC = LY and set STAT interrupt if enabled.
+ */
+#define LYC 0xFF45
+
+
+// ##########################
+// ### Tiles and Palettes ###
+// ##########################
+
+/**
+ * @brief OAM DMA - Contains source from RAM or ROM to copy into OAM, writing to this register starts the DMA transfer.
+ */
+#define DMA 0xFF46
+
+/**
+ * @brief Background Palette Data - Assigns shades of gray for BG and Windows tiles.
+ */
+#define BGP 0xFF47
+
+/**
+ * @brief OBJ Palette 0
+ */
+#define OBP0 0xFF48
+
+/**
+ * @brief OBJ Palette 1
+ */
+#define OBP1 0xFF49
 #endif
