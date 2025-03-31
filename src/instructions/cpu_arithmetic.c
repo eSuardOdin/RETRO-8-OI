55db18e4446ca9b7f83f55af758a0b9291ff3944
RACunderskrt <manuel.humbert42@gmail.com>
2025-03-31 16:13:11 +0200

    inc, dec added

--- src/instructions/cpu_arithmetic.c
+++ src/instructions/cpu_arithmetic.c
@@ -370,3 +370,98 @@ int cp_a_hl(gameboy* gb){
     return result;
 }
 
+/*
+    INC
+*/
+
+/**
+ * @brief Incremente de 1 r8.
+ *
+ * @author Thomas
+ * @param opcode Opcode.
+ * @param gb Instance de GameBoy.
+ */
+ int inc_r8(uint8_t opcode, gameboy* gb){
+    uint8_t *src;
+
+    uint8_t src_byte = opcode & 0x03;
+    src = get_r8(src_byte, gb);
+    uint8_t result = *src + 1;
+    int carry = *src + 1;
+    inc_cycle(gb);
+
+    *src = result;
+
+    set_flags(gb,carry,1,0);
+    return 0;
+}
+
+/**
+ * @brief Incremente de 1 la valeur pointée par HL.
+ *
+ * @author Thomas
+ * @param gb Instance de GameBoy.
+ */
+int inc_hl(gameboy* gb){
+    uint8_t *src = get_address(gb, gb->reg->hl);
+    inc_cycle(gb);
+
+    uint8_t result = *src + 1;
+    int carry = *src + 1;
+    inc_cycle(gb);
+    inc_cycle(gb);
+
+    *src = result;
+    
+    set_flags(gb,carry,1,0);
+    return 0;
+}
+
+/*
+    DEC
+*/
+
+/**
+ * @brief Decremente de 1 r8.
+ *
+ * @author Thomas
+ * @param opcode Opcode.
+ * @param gb Instance de GameBoy.
+ */
+ int dec_r8(uint8_t opcode, gameboy* gb){
+    uint8_t *src;
+
+    uint8_t src_byte = opcode & 0x03;
+    src = get_r8(src_byte, gb);
+    uint8_t result = *src - 1;
+    int carry = *src - 1;
+    inc_cycle(gb);
+
+    *src = result;
+
+    set_flags(gb,carry,1,0);
+    return 0;
+}
+
+/**
+ * @brief Decremente de 1 la valeur pointée par HL.
+ *
+ * @author Thomas
+ * @param gb Instance de GameBoy.
+ */
+int dec_hl(gameboy* gb){
+    uint8_t *src = get_address(gb, gb->reg->hl);
+    inc_cycle(gb);
+
+    uint8_t result = *src - 1;
+    int carry = *src - 1;
+    inc_cycle(gb);
+    inc_cycle(gb);
+
+    *src = result;
+    
+    set_flags(gb,carry,1,0);
+    return 0;
+}
+
+
