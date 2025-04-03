52eee376fa754a70d96db810922ed2bed3870f68
RACunderskrt <manuel.humbert42@gmail.com>
2025-04-03 13:56:04 +0200

    flag carry added

--- src/instructions/cpu_arithmetic.c
+++ src/instructions/cpu_arithmetic.c
@@ -28,7 +28,7 @@ int add_a_r8(uint8_t opcode, gameboy* gb){
 
     *dst = result;
 
-    set_arithmetic_flags(0);
+    set_arithmetic_flags(carry,0);
     return 0;
 }
 
@@ -44,7 +44,7 @@ int add_a_hl(gameboy* gb){
 
     *dst = result;
     
-    set_arithmetic_flags(0);
+    set_arithmetic_flags(carry,0);
     return 0;
 }
 
@@ -61,7 +61,7 @@ int add_a_n(gameboy* gb){
 
     gb->reg->a = result;
     
-    set_arithmetic_flags(0);
+    set_arithmetic_flags(carry,0);
     return 0;
 }
 
@@ -85,7 +85,7 @@ int adc_a_r8(uint8_t opcode, gameboy* gb){
 
     *dst = result;
 
-    set_arithmetic_flags(0);
+    set_arithmetic_flags(carry,0);
     return 0;
 }
 
@@ -102,7 +102,7 @@ int adc_a_hl(gameboy* gb){
 
     *dst = result;
     
-    set_arithmetic_flags(0);
+    set_arithmetic_flags(carry,0);
     return 0;
 }
 
@@ -120,7 +120,7 @@ int adc_a_n(gameboy* gb){
 
     gb->reg->a = result;
     
-    set_arithmetic_flags(0);
+    set_arithmetic_flags(carry,0);
     return 0;
 }
 
@@ -143,7 +143,7 @@ int sub_a_r8(uint8_t opcode, gameboy* gb){
 
     *dst = result;
 
-    set_arithmetic_flags(1);
+    set_arithmetic_flags(carry,1);
     return 0;
 }
 
@@ -159,7 +159,7 @@ int sub_a_hl(gameboy* gb){
 
     *dst = result;
     
-    set_arithmetic_flags(1);
+    set_arithmetic_flags(carry,1);
     return 0;
 }
 
@@ -176,7 +176,7 @@ int sub_a_n(gameboy* gb){
 
     gb->reg->a = result;
     
-    set_arithmetic_flags(1);
+    set_arithmetic_flags(carry,1);
     return 0;
 }
 
@@ -200,7 +200,7 @@ int sbc_a_r8(uint8_t opcode, gameboy* gb){
 
     *dst = result;
 
-    set_arithmetic_flags(1);
+    set_arithmetic_flags(carry,1);
     return 0;
 }
 
@@ -217,7 +217,7 @@ int sbc_a_hl(gameboy* gb){
 
     *dst = result;
     
-    set_arithmetic_flags(1);
+    set_arithmetic_flags(carry,1);
     return 0;
 }
 
@@ -235,7 +235,7 @@ int sbc_a_n(gameboy* gb){
 
     gb->reg->a = result;
     
-    set_arithmetic_flags(1);
+    set_arithmetic_flags(carry,1);
     return 0;
 }
 
@@ -257,7 +257,7 @@ int cp_a_r8(uint8_t opcode, gameboy* gb){
     int carry = *src - *dst;
     inc_cycle(gb);
 
-    set_arithmetic_flags(1);
+    set_arithmetic_flags(carry,1);
     return result;
 }
 
@@ -271,7 +271,7 @@ int cp_a_hl(gameboy* gb){
     int carry = *src - *dst;
     inc_cycle(gb);
     
-    set_arithmetic_flags(1);
+    set_arithmetic_flags(carry,1);
     return result;
 }
 
@@ -286,7 +286,7 @@ int cp_a_n(gameboy* gb){
     int carry = src - dst;
     inc_cycle(gb);
     
-    set_arithmetic_flags(1);
+    set_arithmetic_flags(carry,carry,1);
     return result;
 }
 
@@ -380,7 +380,7 @@ int dec_hl(gameboy* gb){
 =====================
 */
 
-void set_arithmetic_flags(int is_sub){
+void set_arithmetic_flags(int carry, int is_sub){
     set_Z_flags(gb,carry);
     set_N_flag(gb, is_sub);
     set_H_flag(gb, *src, *dst, 1);
