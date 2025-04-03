3e1916d78c7869e2bdad917930f00cec7c651a52
RACunderskrt <manuel.humbert42@gmail.com>
2025-04-03 13:35:21 +0200

    fix set flag

--- src/gameboy.c
+++ src/gameboy.c
@@ -264,18 +264,24 @@ int set_flags(gameboy* gb, unsigned int result, int is_8bit, int is_sub){
 int set_Z_flags(gameboy* gb, unsigned int result){
     if(!result)
         gb->reg->f = gb->reg->f | 0b10000000;
+    else
+        gb->reg->f = gb->reg->f | 0b01111111;
     return 0;
 }
 
 int set_N_flag(gameboy* gb, int is_sub){
     if(is_sub)
         gb->reg->f = gb->reg->f | 0b01000000;
+    else
+        gb->reg->f = gb->reg->f & 0b10111111;
     return 0;
 }
 
 int set_C_flag(gameboy* gb, unsigned int result, int is_8bit){
     if((is_8bit && (result > 255)) || (!is_8bit && (result > 65535)))
         gb->reg->f = gb->reg->f | 0b00010000;
+    else
+        gb->reg->f = gb->reg->f & 0b11101111;
     return 0;
 }
 
