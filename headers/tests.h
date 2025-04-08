b65c45b844c4db8487623a19655d6ac2130ba4f1
RACunderskrt <manuel.humbert42@gmail.com>
2025-04-08 11:44:22 +0200

    tests added

---
+++ headers/tests.h
@@ -0,0 +1,36 @@
+/**
+ * @file cartridge.h
+ * @author Thomas HUMBERT (RACé)
+ * @brief ça fait des tests
+ * @version 0.1
+ * @date 2025-03-25
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+ #ifndef CARTRIDGE_H
+ #define CARTRIDGE_H
+ 
+ #include <stdint.h>
+
+/**
+ * @brief Display the value of the reg
+ */
+void display_reg(gameboy* gb);
+
+/**
+ * @brief Put every reg at 0
+ */
+void reset_reg(gameboy* gb);
+
+/**
+ * @brief test the add_r8 function
+ */
+void test_add_r8(gameboy* gb);
+
+/**
+ * @brief test the sub_r8 function
+ */
+void test_sub_r8(gameboy* gb);
+
+#endif
---
+++ src/tests.c
@@ -0,0 +1,87 @@
+#include "../headers/gameboy.h"
+#include "../headers/instructions/cpu_arithmetic.h"
+#include "../headers/tests.h"
+#include <stdint.h>
+#include <stdio.h>
+
+void display_reg(gameboy* gb){
+    printf("=====================\n");
+    printf("a :%d\n", gb->reg->a);
+    printf("b :%d\n", gb->reg->b);
+    printf("c :%d\n", gb->reg->c);
+    printf("d :%d\n", gb->reg->d);
+    printf("e :%d\n", gb->reg->e);
+    printf("=====================\n");
+}
+
+void reset_reg(gameboy* gb){
+    gb->reg->a = 0;
+    gb->reg->b = 0;
+    gb->reg->c = 0;
+    gb->reg->d = 0;
+    gb->reg->e = 0;
+}
+
+void test_add_r8(gameboy* gb){
+    printf("ton pere\n");
+    printf("ADD A+B\n");
+
+    gb->reg->b = 5;
+    display_reg(gb);
+    
+    uint8_t opcode = 0b10000000;
+    add_a_r8(opcode,gb);
+    display_reg(gb);
+
+    reset_reg(gb);
+    printf("ADD A+C\n");
+    gb->reg->c = 3;
+    display_reg(gb);
+
+    opcode = 0b10000001;
+    add_a_r8(opcode,gb);
+    display_reg(gb);
+
+    reset_reg(gb);
+    printf("ADD A+D\n");
+    gb->reg->d = 2;
+    display_reg(gb);
+
+    opcode = 0b10000010;
+    add_a_r8(opcode,gb);
+    display_reg(gb);
+    reset_reg(gb);
+}
+
+void test_sub_r8(gameboy* gb){
+    printf("ton pere\n");
+    printf("ADD A-B\n");
+    gb->reg->a = 10;
+    gb->reg->b = 5;
+    display_reg(gb);
+    
+    uint8_t opcode = 0b10010000;
+    sub_a_r8(opcode,gb);
+    display_reg(gb);
+
+    reset_reg(gb);
+    printf("ADD A-C\n");
+    gb->reg->a = 10;
+    gb->reg->c = 3;
+    display_reg(gb);
+
+    opcode = 0b10010001;
+    sub_a_r8(opcode,gb);
+    display_reg(gb);
+
+    reset_reg(gb);
+    printf("ADD A-D\n");
+    gb->reg->a = 10;
+    gb->reg->d = 2;
+    display_reg(gb);
+
+    opcode = 0b10010010;
+    sub_a_r8(opcode,gb);
+    display_reg(gb);
+    
+}
